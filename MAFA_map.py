0966e5bd5fac02f7fba45c8489ae7ddb1eff8300
Thomas Giannetti-Fakhoury <72355918+thomasgiannetti@users.noreply.github.com>
2024-05-14 00:16:25 +0000

    Update MAFA_map.py

--- MAFA_map.py
+++ MAFA_map.py
@@ -9,42 +9,37 @@ import folium
 from streamlit_folium import st_folium
 import streamlit as st
 from streamlit_folium import folium_static
-from folium.plugins import MarkerCluster
 
 df = pd.read_csv('Folium Map FINAL - Feuille 1.csv')
 
 def create_map():
-    m = folium.Map(location=[4.74851, -6.6363], zoom_start=12)
+  m = folium.Map(location=[4.74851, -6.6363], zoom_start=12)
 
-    # Create a MarkerCluster layer with custom options
-    marker_cluster = MarkerCluster(disable_clustering_at_zoom=14).add_to(m)
-    # Adjust the disable_clustering_at_zoom according to your preference
+  for index, row in df.iterrows():
+    iframe_content = (
+        f"<b>Entreprenant:</b> {row['Entreprenant/Display Name']} <br><br>"
+        f"<b>Contact:</b> {row['Bon numéro de téléphone']} <br><br>"
+        f"<b>Nom de l'activité:</b> {row['Denomination_ou_raison_sociale']} <br>"
+        f"<b>Date de naissance:</b> {row['Entreprenant/Date de naissance']} <br>"
+        f"<b>Nature de l'activité:</b> {row['Type_activite']} <br><br>"
+        f"<b>Chiffre d'affaire:</b> {row['chiffre_affaire']} <br>"
+        f"<b>Taille de l'activité:</b> {row['taille_activite']} <br>"
+    )
 
-    for index, row in df.iterrows():
-        iframe_content = (
-  
-            f"<b>Entreprenant:</b> {row['Entreprenant/Display Name']} <br><br>"
-            f"<b>Contact:</b> {row['Bon numéro de téléphone']} <br><br>"
-            f"<b>Nom de l'activité:</b> {row['Denomination_ou_raison_sociale']} <br>"
-            f"<b>Date de naissance:</b> {row['Entreprenant/Date de naissance']} <br>"
-            f"<b>Nature de l'activité:</b> {row['Type_activite']} <br><br>"
-            f"<b>Chiffre d'affaire:</b> {row['chiffre_affaire']} <br>"
-            f"<b>Taille de l'activité:</b> {row['taille_activite']} <br>"
-        )
-        popup = folium.Popup(iframe_content, min_width=300, max_width=300)
-        if row['Etat'] == "Annulé/Rejeté":
-            marker_color = 'red'
-        elif row['Etat'] == "Confirmé":
-            marker_color = 'orange'
-        elif row['Etat'] == "Validé":
-            marker_color = 'green'
-        else:
-            marker_color = 'gray'
-
-        # Add the marker to the MarkerCluster layer
-        folium.Marker(location=[row['Géolatitude'], row['Géolongitude']], icon=folium.Icon(color=marker_color, icon='map-marker', prefix='fa'), popup=popup).add_to(marker_cluster)
-
-    return m
+    popup = folium.Popup(iframe_content, min_width=300, max_width=300)
     
+    if row['Etat'] == "Annulé/Rejeté":
+        marker_color = 'red'
+    elif row['Etat'] == "Confirmé":
+        marker_color = 'orange'
+    elif row['Etat'] == "Validé":
+        marker_color = 'green'
+    else:
+        marker_color = 'gray' 
+    
+    folium.Marker(location=[row['Géolatitude'], row['Géolongitude']], icon=folium.Icon(color=marker_color, icon='map-marker', prefix='fa'), popup=popup).add_to(m) 
+  return m
+
+
 map = create_map()   
 folium_static(map, width=750)
